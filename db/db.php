8033b1f7883c3ae7b53803457d298faaedd8a70b
Lars Fredrik Holst-Try <lars.fr.97@hotmail.com>
2025-05-03 22:46:06 +0200

    use localhost

--- db/db.php
+++ db/db.php
@@ -4,15 +4,15 @@ if(!isset($_SESSION)) {
     session_start(); 
 } 
 
-$servername = "cpanel80.proisp.no";
-$user = "mafiovrx";
-$pass = "82vH84K24g9m+8";
+$servername = "localhost";
+$user = "root";
+$pass = "";
 $db = "mafiovrx_mafioso2";
 
 $con = mysqli_connect($servername, $user, $pass, $db);
 
 if (mysqli_connect_errno()){
-    echo "Failed to connect to MySQL: "; //. mysqli_connect_error();
+    echo "Failed to connect to MySQL: ";
 }
 
 ?>
